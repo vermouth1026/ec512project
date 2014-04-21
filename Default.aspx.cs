aad1adb494aff4fc7a2b2355a9f2df4dd87cff43
Yuchen Huang <yuchen.hust@gmail.com>
2014-04-21 11:10:20 -0400

    add comment div disable

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -102,14 +102,18 @@ public partial class _Default : System.Web.UI.Page
         int dis_num = 5;
         SqlConnection conn = new SqlConnection(connString);
         conn.Open();
-        string cmdstr = "select top 5 c.Comments,c.Email,c.Image_Url,c.Person_Id,c.Name,COURSE.Code,COURSE.Name as Course_Name,COURSE.Score_Overall from ((select a.Course_Id,a.Comments,a.Datetime,a.Person_Id,b.Email,b.Name,b.Image_Url from RATING a inner join PERSON b on a.Person_Id=b.Id ) c inner join COURSE on COURSE.Id=c.Course_Id) order by c.Datetime desc"; 
+        string cmdstr = "select c.Comments,c.Email,c.Image_Url,c.Person_Id,c.Name,COURSE.Code,COURSE.Name as Course_Name,COURSE.Score_Overall, COURSE.Score_Contents, COURSE.Score_Hardness, COURSE.Score_Professor from ((select a.Course_Id,a.Comments,a.Datetime,a.Person_Id,b.Email,b.Name,b.Image_Url from RATING a inner join PERSON b on a.Person_Id=b.Id ) c inner join COURSE on COURSE.Id=c.Course_Id) order by c.Datetime desc"; 
         SqlCommand cmd = new SqlCommand(cmdstr, conn);
         SqlDataReader rdr = cmd.ExecuteReader();
         int i = 0;
         string[] courseCode = new string[dis_num];
         string[] courseName = new string[dis_num];
         string[] text = new string[dis_num];
-        string[] score = new string[dis_num];
+        string[] score0 = new string[dis_num];
+        string[] score1 = new string[dis_num];
+        string[] score2 = new string[dis_num];
+        string[] score3 = new string[dis_num];
+
         string[] student_name = new string[dis_num];
         string[] email = new string[dis_num];
         string[] profile = new string[dis_num];
@@ -125,7 +129,7 @@ public partial class _Default : System.Web.UI.Page
             courseCode[i] = (string)rdr["Code"];
             courseName[i] = (string)rdr["Course_Name"];
             text[i] = (string)rdr["Comments"];
-            score[i] = Convert.ToString(rdr["Score_Overall"]);
+            score0[i] = Convert.ToString(rdr["Score_Overall"]).Substring(0,1);
             student_name[i] = (string)rdr["Name"];
             email[i] = (string)rdr["Email"]+"@bu.edu";
             img_pos[i] = (string)rdr["Image_Url"];
@@ -133,13 +137,29 @@ public partial class _Default : System.Web.UI.Page
             i++;
         }
 
+        
+        switch(i)
+        {
+            case 0:
+                rr1.Visible = false; rr2.Visible = false; rr3.Visible = false; rr4.Visible = false; rr5.Visible = false; break;
+            case 1:
+                rr2.Visible = false; rr3.Visible = false; rr4.Visible = false; rr5.Visible = false; break;
+            case 2:
+                rr3.Visible = false; rr4.Visible = false; rr5.Visible = false; break;
+            case 3:
+                rr4.Visible = false; rr5.Visible = false; break;
+            case 4:
+                rr5.Visible = false; break;
+            default: break;
+        }
+        
 
         for(int j = 0; j < dis_num; j++)
         {
             img[j] = "<img src=\"" + img_pos[j] + "\" alt=\"profile\" width = 150 />";
             //img[j] = "<p>" + img_pos[j] + "</p>";
             name_link[j] = "<a href = \"user.aspx?id=" + student_id[j] + "\" > " + student_name[j] + "</a>";
-            course_link[j] = userPage + "?id=" + courseCode[j];
+            course_link[j] = coursePage + "?id=" + courseCode[j];
             info[j] = img[j] + "</br>" + name_link[j] + "</br>" + email[j];
         }
 
@@ -149,14 +169,20 @@ public partial class _Default : System.Web.UI.Page
         profile1.InnerHtml = info[0];
         rcc1.Text = courseCode[0] + " " + courseName[0];
         rcc1.NavigateUrl = course_link[0];
-        rcs1_0.Text = score[0];
+        rcs1_0.Text = "&nbsp Total: " + score0[0] + " &nbsp";
+        rcs1_1.Text = "&nbsp Prof: " + score1[0] + " &nbsp";
+        rcs1_2.Text = "&nbsp Cont: " + score2[0] + " &nbsp";
+        rcs1_3.Text = "&nbsp Hard: " + score3[0] + " &nbsp";
         comment1.Text = text[0];
 
 
         profile2.InnerHtml = info[1];
         rcc2.Text = courseCode[1] + " " + courseName[1];
         rcc2.NavigateUrl = course_link[1];
-        rcs2_0.Text = score[1];
+        rcs2_0.Text = "&nbsp Total: " + score0[1] + " &nbsp";
+        rcs2_1.Text = "&nbsp Prof: " + score1[1] + " &nbsp";
+        rcs2_2.Text = "&nbsp Cont: " + score2[1] + " &nbsp";
+        rcs2_3.Text = "&nbsp Hard: " + score3[1] + " &nbsp";
         comment2.Text = text[1];
 
 
@@ -164,7 +190,10 @@ public partial class _Default : System.Web.UI.Page
         profile3.InnerHtml = info[2];
         rcc3.Text = courseCode[2] + " " + courseName[2];
         rcc3.NavigateUrl = course_link[2];
-        rcs3_0.Text = score[2];
+        rcs3_0.Text = "&nbsp Total: " + score0[2] + " &nbsp";
+        rcs3_1.Text = "&nbsp Prof: " + score1[2] + " &nbsp";
+        rcs3_2.Text = "&nbsp Cont: " + score2[2] + " &nbsp";
+        rcs3_3.Text = "&nbsp Hard: " + score3[2] + " &nbsp";
         comment3.Text = text[2];
 
 
@@ -172,7 +201,10 @@ public partial class _Default : System.Web.UI.Page
         profile4.InnerHtml = info[3];
         rcc4.Text = courseCode[3] + " " + courseName[3];
         rcc4.NavigateUrl = course_link[3];
-        rcs4_0.Text = score[3];
+        rcs4_0.Text = "&nbsp Total: " + score0[3] + " &nbsp";
+        rcs4_1.Text = "&nbsp Prof: " + score1[3] + " &nbsp";
+        rcs4_2.Text = "&nbsp Cont: " + score2[3] + " &nbsp";
+        rcs4_3.Text = "&nbsp Hard: " + score3[3] + " &nbsp";
         comment4.Text = text[3];
 
 
@@ -180,10 +212,14 @@ public partial class _Default : System.Web.UI.Page
         profile5.InnerHtml = info[4];
         rcc5.Text = courseCode[4] + " " + courseName[4];
         rcc5.NavigateUrl = course_link[4];
-        rcs5_0.Text = score[4];
-        comment5.Text = text[4];     
-        
+        rcs5_0.Text = "&nbsp Total: " + score0[4] + " &nbsp";
+        rcs5_1.Text = "&nbsp Prof: " + score1[4] + " &nbsp";
+        rcs5_2.Text = "&nbsp Total: " + score2[4] + " &nbsp";
+        rcs5_3.Text = "&nbsp Total: " + score3[4] + " &nbsp";
+        comment5.Text = text[4];             
     }
+
+
     protected void logout_Click(object sender, EventArgs e)
     {
 
